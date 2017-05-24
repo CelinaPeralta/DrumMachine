a9f94db883a276ca4f455633330180024e05b68a
CelinaPeralta <celper19@bergen.org>
2017-05-24 10:13:31 -0400

    it's lit

--- src/Tester.java
+++ src/Tester.java
@@ -16,16 +16,17 @@ import java.net.URL;
 
 public class Tester {
 
+    private static Player player;
 
     public static void main(String[] args) throws Exception {
-
-        Sound sound = new Sound("HiHat1.wav");
-        Player player = new Player();
+        
+        player = new Player();
 
         boolean[] beat1 = new boolean[16];
         boolean[] beat2 = new boolean[16];
         boolean[] beat3 = new boolean[16];
-
+        boolean[] beat4 = new boolean[16];
+        boolean[] beat5 = new boolean[16];
 
         for (int i = 0; i < 16; i++) {
             if (i % 7 == 1) beat1[i] = true;
@@ -33,32 +34,36 @@ public class Tester {
             if (i % 6 == 0) beat2[i] = true;
             if (i % 3 == 0) beat2[i] = true;
             beat3[i] = true;
+            beat4[i] = (Math.random() < 0.5);
+            beat5[i] = (Math.random() < 0.5);
         }
 
         player.addLoop(1, beat1);
         player.addLoop(2, beat2);
         player.addLoop(0, beat3);
 
-        for (int i = 0; i < 64; i++) {
-            player.play();
-            Thread.sleep(200);
-        }
+        playLoop(2, 200);
 
         player.clearLoop(0);
+        player.addLoop(2, beat4);
 
-        for (int i = 0; i < 32; i++) {
-            player.play();
-            Thread.sleep(200);
-        }
+        playLoop(1, 200);
 
+        player.addLoop(2, beat5);
+
+        playLoop(1, 200);
+
+        player.addLoop(2, beat2);
         player.addLoop(0, beat3);
 
-        for (int i = 0; i < 64; i++) {
+        playLoop(2, 200);
+    }
+
+    private static void playLoop(int bars, int tempo) throws Exception {
+        for (int i = 0; i < bars * 16; i++) {
             player.play();
-            Thread.sleep(200);
+            Thread.sleep(tempo);
         }
-
-
     }
 
 }
