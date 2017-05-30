6a9bad79c203ca6c3b223fbbd503fe652c1bc230
briancho24 <briancho24@yahoo.com>
2017-05-30 08:59:44 -0400

    controlPanel

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -12,34 +12,37 @@ import java.awt.event.ActionListener;
 
 public class ControlPanel extends JPanel{
 
-    private Instrument[] instruments;
+    private int currentInstrument;
     private int tempo;         //in BPM
     private int timeSignature;  //beats per measure and each beat is a quarter note
     private boolean isPlaying = false;
 
-    public ControlPanel(Instrument[] instruments){
-        this.instruments = instruments;
+    public ControlPanel(){
         tempo = 120;
         timeSignature = 4;
 
-        setLayout(new GridLayout(3, 2));
+        setLayout(new GridLayout(4, 2));
 
-        JSlider tempoBar = new JSlider(40,300, tempo);
-        tempoBar.addChangeListener(new TempoChangeListener());
+        JSlider tempoSlider = new JSlider(40,300, tempo);
+        tempoSlider.addChangeListener(new TempoChangeListener());
 
-        add(tempoBar);
+        add(tempoSlider);
         add(new JLabel("Tempo"));
 
-        JRadioButton time3 = new JRadioButton("4", true);
-        JRadioButton time4 = new JRadioButton("3");
-        time3.setActionCommand("3");
+        JSlider instrumentSlider = new JSlider(0,10, 0);
+        instrumentSlider.setMajorTickSpacing(1);
+        instrumentSlider.addChangeListener(new InstrumentChangeListener());
+
+        JRadioButton time4 = new JRadioButton("4", true);
+        JRadioButton time3 = new JRadioButton("3");
         time4.setActionCommand("4");
+        time3.setActionCommand("3");
 
         time3.addActionListener(new TimeActionListener());
 
         ButtonGroup time = new ButtonGroup();
-        time.add(time3);
         time.add(time4);
+        time.add(time3);
 
         JPanel timePanel = new JPanel(new GridLayout(2,1));
         timePanel.add(time3);
@@ -48,12 +51,13 @@ public class ControlPanel extends JPanel{
         add(timePanel);
         add(new JLabel("Time Signature"));
 
+
         JToggleButton startButton = new JToggleButton("Start", false);
         startButton.setActionCommand("start");
 
         startButton.addChangeListener(new StartChangeListener());
-
     }
+
 
     public class TempoChangeListener implements ChangeListener{
 
@@ -79,6 +83,17 @@ public class ControlPanel extends JPanel{
         @Override
         public void stateChanged(ChangeEvent e) {
             isPlaying = true;
+        }
+    }
+
+    public class InstrumentChangeListener implements ChangeListener{
+
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            JSlider root = (JSlider)e.getSource();
+            if(!root.getValueIsAdjusting()){
+                currentInstrument = (int)root.getValue();
+            }
         }
     }
 }
