344254ae41e8ad90d0a2573e685505b275821985
CelinaPeralta <celper19@bergen.org>
2017-06-01 11:47:16 -0400

    big ole button

--- src/DrumMachineUI.java
+++ src/DrumMachineUI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by brian on 5/29/2017.
@@ -9,6 +11,8 @@ public class DrumMachineUI extends JFrame {
     public static Player player;
     public static RhythmPanel rhythmPanel;
     public static ControlPanel controlPanel;
+    public static JButton start;
+    public static Thread looper;
 
     public DrumMachineUI() throws Exception {
 
@@ -29,6 +33,20 @@ public class DrumMachineUI extends JFrame {
         add(controlPanel, BorderLayout.WEST);
         //add(mixerPanel, BorderLayout.CENTER);
         add(rhythmPanel, BorderLayout.SOUTH);
+        start = new JButton("Start/Stop");
+        start.setEnabled(true);
+        add(start);
+
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(looper.isAlive())
+                    looper.stop();
+                else
+                    looper.start();
+
+            }
+        });
     }
 
     public static void main(String[] args) throws Exception {
@@ -40,17 +58,20 @@ public class DrumMachineUI extends JFrame {
         frame.setVisible(true);
 
         LoopThread loop = new LoopThread();
-        loop.setDaemon(true);
-
-        UpdateThread updateThread = new UpdateThread();
-        updateThread.setDaemon(true);
+        looper = new Thread(loop);
+        looper.setDaemon(true);
 
 //        updateThread.run();
-        loop.run();
+        if (start.isSelected()) {
+            if (looper.isAlive())
+                looper.interrupt();
+            else
+                looper.start();
+        }
 
     }
 
-    public static class LoopThread extends Thread {
+    public static class LoopThread implements Runnable {
         @Override
         public void run() {
             while (true) {
@@ -66,19 +87,5 @@ public class DrumMachineUI extends JFrame {
         }
     }
 
-    public static class UpdateThread extends Thread {
-        @Override
-        public void run() {
-            while (true) {
-                rhythmPanel.setInstrument(controlPanel.getCurrentInstrument());
-                System.out.println("update");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
 
-            }
-        }
-    }
 }
